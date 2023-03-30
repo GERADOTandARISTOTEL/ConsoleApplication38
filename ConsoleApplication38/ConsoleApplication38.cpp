7a2a161d549bcb551a90ad2df01e046ca1440244
steam <steam@DESKTOP-FOFIITB>
2023-03-30 19:53:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication38/ConsoleApplication38.cpp
@@ -0,0 +1,346 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+void print()
+{
+	cout << "\t\t\t\t\t                  ¶¶" << endl;
+	cout << "\t\t\t\t\t                  ¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                  ¶¶" << endl;
+	cout << "\t\t\t\t\t\                  ¶¶" << endl;
+	cout << "\t\t\t\t\t\                  ¶¶" << endl;
+	cout << "\t\t\t\t\t\                  ¶¶" << endl;
+	cout << "\t\t\t\t\t\                  ¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                 ¶¶¶¶  " << endl;
+	cout << "\t\t\t\t\t\                ¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                ¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                ¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                ¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\                ¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\               ¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\               ¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\               ¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\               ¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\              ¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\              ¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\            ¶¶¶¶¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\            ¶¶¶¶¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\             ¶¶¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\             ¶¶¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\             ¶¶¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\            ¶¶¶¶¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\            ¶¶¶¶¶     ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\           ¶¶¶¶¶      ¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\           ¶¶¶¶¶       ¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\          ¶¶¶¶¶¶       ¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\         ¶¶¶¶¶¶        ¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\      ¶¶¶¶¶¶¶¶¶_¶_¶_¶_¶_¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\      ¶¶¶¶¶¶¶¶¶_¶_¶_¶_¶_¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\      ¶¶¶¶¶¶¶¶¶_¶_¶_¶_¶_¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\      ¶¶¶¶¶¶¶¶¶_¶_¶_¶_¶_¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\      ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\    ¶¶¶¶¶¶¶¶¶¶__________¶¶¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\   ¶¶¶¶¶¶¶¶________________¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\  ¶¶¶¶¶¶¶¶__________________¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t\ ¶¶¶¶¶¶¶¶____________________¶¶¶¶¶¶¶¶" << endl;
+	cout << "\t\t\t\t\t¶¶¶¶¶¶¶¶______________________¶¶¶¶¶¶¶¶" << endl;
+}
+void field(string(&a2)[10][10], string(&b2)[10][10]) {
+	cout << endl;
+	cout << "                                                    " << "Морской бой" << endl;
+	cout << endl << endl << endl;
+	for (int a = 0; a < 1; a++) {
+		cout << "                             ";
+		for (int b = 0; b < size(a2); b++) {
+			cout << b << " ";
+		}
+		cout << "                   ";
+		for (int b = 0; b < size(a2); b++) {
+			cout << b << " ";
+		}
+		cout << endl;
+		for (int i = 0; i < size(a2); i++) {
+			cout << "                           ";
+			cout << i;
+			for (int j = 0; j < size(a2); j++) {
+				cout << "|" << a2[i][j];
+
+			}
+			cout << "|" << "                 ";
+			cout << i;
+			for (int f = 0; f < size(b2); f++) {
+				cout << "|" << b2[i][f];
+
+			}
+			cout << "|" << endl;
+
+		}
+	}
+
+}
+void help(short& l, short& s, string(&a2)[10][10], string(&b2)[10][10]) {
+	cout << endl;
+	cout << "Выберите местоположение ваших кораблей" << endl << "Корабли выставляются по возрастанию: сначала однопалубный после двупалубный и последним трёхпалубный" << endl;
+	cout << "Введите координаты и поставьте знаки " << endl;
+	cout << "Знак" << '"' << "1" << '"' << " " << "чтобы корабль стоял носом наверх\n";
+	cout << "Знак" << '"' << "2" << '"' << " " << "чтобы корабль стоял носом влево\n";
+	cout << "Знак" << '"' << "3" << '"' << " " << "чтобы корабль стоял носом вправо \n";
+	cout << "Знак" << '"' << "4" << '"' << " " << "чтобы корабль стоял носом вниз  \n";
+	cout << "введите столбик:"; cin >> l;
+	cout << "введите строку: "; cin >> s;
+	int tu = 10;
+	while (tu != 0) {
+		tu = 0;
+		if (s > 9 || l > 9) {
+			tu = 1;
+			cout << "\t\t\t\t\t\tНеверные координаты";
+			Sleep(2000);
+			system("cls");
+			field(a2, b2);
+			help(l, s, a2, b2);
+		}
+		else if (s < 0 || l < 0) {
+			tu = 1;
+			cout << "\t\t\t\t\t\tНеверные координаты";
+			Sleep(2000);
+			system("cls");
+			field(a2, b2);
+			help(l, s, a2, b2);
+		}
+
+	}
+}
+int game(string a[10][10], string b[10][10]) {
+	char t;
+	short s, l;
+	string a2[10][10];
+	string b2[10][10];
+	for (char i = 0; i < size(a2); i++) {
+		for (char j = 0; j < size(a2); j++) {
+			a2[i][j] = " ";
+			b2[i][j] = " ";
+			a[i][j] = "*";
+			b[i][j] = "*";
+		}
+
+	}
+	field(a2, b2);
+	help(l, s, a2, b2);
+	a[s][l] = "1";
+	a2[s][l] = '^';
+
+	system("cls");
+
+	field(a2, b2);
+	help(l, s, a2, b2);
+	cout << "введите направление:"; cin >> t;
+	switch (t) {
+	case '1':
+		if (s - 1 >= 0) {
+			a[s][l] = "1";
+			a2[s][l] = "O";
+			a[s - 1][l] = "1";
+			a2[s - 1][l] = "^";
+		}
+		break;
+	case '2':
+		if (l + 1 <= 9) {
+			a[s][l] = "1";
+			a2[s][l] = "O";
+			a[s][l + 1] = "1";
+			a2[s][l + 1] = ">";
+		}
+		break;
+	case '3':
+		if (l - 1 >= 0) {
+			a[s][l] = "1";
+			a2[s][l] = "O";
+			a[s][l - 1] = "1";
+			a2[s][l - 1] = "<";
+		}
+		break;
+	case '4':
+		if (s + 1 <= 9) {
+			a[s][l] = "1";
+			a2[s][l] = "O";
+			a[s + 1][l] = "1";
+			a2[s + 1][l] = "o";
+		}
+	}
+
+	system("cls");
+	field(a2, b2);
+	help(l, s, a2, b2);
+	cout << "введите направление:"; cin >> t;
+
+	switch (t) {
+	case '^':
+		if (s - 2 >= 0) {
+			a[s][l] = "1";
+			a2[s][l] = "O";
+			a[s - 1][l] = "1";
+			a2[s - 1][l] = "O";
+			a[s - 2][l] = "1";
+			a2[s - 2][l] = "^";
+		}
+		break;
+	case '>':
+		if (l + 2 <= 9) {
+			a[s][l] = "1";
+			a2[s][l] = "O";
+			a[s][l + 1] = "1";
+			a2[s][l + 1] = "O";
+			a[s][l + 2] = "1";
+			a2[s][l + 2] = ">";
+		}
+		break;
+	case '<':
+		if (l - 2 >= 0) {
+			a[s][l] = "1";
+			a2[s][l] = "O";
+			a[s][l - 1] = "1";
+			a2[s][l - 1] = "O";
+			a[s][l - 2] = "1";
+			a2[s][l - 2] = "<";
+		}
+		break;
+	case '!':
+		if (s + 2 <= 9) {
+			a[s][l] = "1";
+			a2[s][l] = "O";
+			a[s + 1][l] = "1";
+			a2[s + 1][l] = "O";
+			a[s + 2][l] = "1";
+			a2[s + 2][l] = "o";
+		}
+
+	}
+
+	system("cls");
+
+	field(a2, b2);
+	srand(time(NULL));
+
+	int TAB_BOT, LINE_BOT;
+	TAB_BOT = rand() % 10;
+	LINE_BOT = rand() % 10;
+	b[TAB_BOT][LINE_BOT] = "1";
+	Sleep(200);
+	int u = 0;
+	l = 0;
+	while (u != 1) {
+		TAB_BOT = rand() % 10;
+		LINE_BOT = rand() % 10;
+
+		if (TAB_BOT - 1 <= 9 && LINE_BOT - 1 <= 9) {
+			if (b[TAB_BOT][LINE_BOT] != "1" && b[TAB_BOT - 1][LINE_BOT] != "1") {
+				b[TAB_BOT][LINE_BOT] = "1";
+				b[TAB_BOT - 1][LINE_BOT] = "1";
+				u = 1;
+
+			}
+		}
+	}
+	Sleep(200);
+	l = 0;
+	while (l != 1) {
+		TAB_BOT = rand() % 10;
+		LINE_BOT = rand() % 10;
+
+		if (TAB_BOT + 1 <= 9 && TAB_BOT + 2 <= 9) {
+			if (b[TAB_BOT][LINE_BOT] != "1" && b[TAB_BOT + 1][LINE_BOT] != "1") {
+				if (b[TAB_BOT + 2][LINE_BOT] != "1")
+					b[TAB_BOT][LINE_BOT] = "1";
+				b[TAB_BOT + 1][LINE_BOT] = "1";
+				b[TAB_BOT][LINE_BOT] = "1";
+				b[TAB_BOT + 2][LINE_BOT] = "1";
+				l++;
+
+			}
+		}
+	}
+	short bot_heat = 0, person_heat = 0;
+
+	system("cls");
+	while (bot_heat < 6 && person_heat < 6) {
+
+		field(a2, b2);
+		cout << endl;
+		cout << "Ваш ход , введите координаты\n";
+		cout << "Введите номер столбца:"; cin >> l;
+		cout << "Введите номер строки: "; cin >> s;
+
+		if (b[s][l] == "1") {
+			person_heat++;
+			b2[s][l] = "*";
+			cout << endl;
+			cout << "вы попали";
+		}
+		else {
+			b2[s][l] = "x";
+			cout << "вы промахнулись";
+		}
+		system("cls");
+		field(a2, b2);
+		cout << endl;
+		cout << "ход противника";
+		Sleep(300);
+		l = 1;
+		while (l != 0) {
+			TAB_BOT = rand() % 10;
+			LINE_BOT = rand() % 10;
+
+			if (a2[TAB_BOT][LINE_BOT] != "x" && a2[TAB_BOT][LINE_BOT] != "*") {
+				if (a[TAB_BOT][LINE_BOT] == "1") {
+					bot_heat++;
+					a2[TAB_BOT][LINE_BOT] = "*";
+					l = 0;
+				}
+				else {
+					a2[TAB_BOT][LINE_BOT] = "x";
+					l = 0;
+				}
+			}
+		}
+		system("cls");
+	}
+
+	if (bot_heat > person_heat) {
+		cout << "\t\t\t\t\t\tБот победил!";
+		cout << endl;
+		cout << endl;
+		print();
+	}
+	else {
+		cout << "\t\t\t\t\t\t   Вы победили!";
+		cout << endl;
+		cout << endl;
+		print();
+
+		return 0;
+	}
+}
+int main ()
+{
+	setlocale(LC_ALL, "rus");
+	string a[10][10];
+	string b[10][10];
+	game(a, b);
+}
